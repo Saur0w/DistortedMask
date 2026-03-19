a184a216df04245976efb1f889e5562578b77e3f
saur0w <sthapliyal085@gmail.com>
2026-03-19 12:43:27 +0530

    "Mesh Imports"

--- src/components/Landing/Mesh.tsx
+++ src/components/Landing/Mesh.tsx
@@ -1,7 +1,21 @@
 "use client";
 
-import { useRef } from "react";
+import {useRef, useState} from "react";
 import { useFrame, useThree } from "@react-three/fiber";
+import { useTexture } from "@react-three/drei";
 import * as THREE from "three";
 import { vertexShader, fragmentShader} from "@/lib/Shader";
 
+export default function Mesh() {
+    const { viewport } = useThree();
+    const materailRef = useRef<THREE.RawShaderMaterial>(null);
+    const timeRef = useRef(0);
+    const maskVisibility = useRef({ value: 0 });
+    const maskPosition = useRef({ x: 0, y: 0 });
+
+    const [frontTexture, backTexture] = useTexture([
+       "/images/look.jpg",
+       "/images/blue.jpg"
+    ]);
+
+}
